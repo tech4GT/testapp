aba3174e30456f015133421420fe85955da9dc71
tech4GT <varun.gupta1798@gmail.com>
2020-08-19 13:28:53 +0530

    update
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,37 +5,38 @@ import * as Antd from 'antd';
 import * as Mat from '@material-ui/core'
 import * as vic from 'victory'
 import * as vis from 'react-vis';
+import * as recharts from 'recharts'
 
-
-const rechart = require('recharts');
-
+const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
 function App() {
   return (
     <div className="App">
-    <vic.VictoryPie />
-    <Antd.Button>I'm a button</Antd.Button>
-    <Mat.Button>I'm also a button</Mat.Button>
-    <vis.XYPlot
-    width={300}
-    height={300}>
-    </vis.XYPlot>
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-    Edit <code>src/App.js</code> and save to reload.
+      <vic.VictoryPie />
+      <Antd.Button>I'm a button</Antd.Button>
+      <Mat.Button>I'm also a button</Mat.Button>
+      <vis.XYPlot
+        width={300}
+        height={300}>
+      </vis.XYPlot>
+      <recharts.LineChart width={400} height={400} data={data}>
+
+      </recharts.LineChart>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
     </p>
-    <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-    Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
     </a>
-    </header>
+      </header>
     </div>
-    );
-  }
-  
-  export default App;
-  
+  );
+}
+
+export default App;
